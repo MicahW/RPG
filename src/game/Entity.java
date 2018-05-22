d479ca943965429a9198b78f03a8f622c99ad20b
demostheneso <demostheneso25@gmail.com>
2018-05-22 10:08:44 -0400

    started to create classes for entitys

---
+++ src/game/Entity.java
@@ -0,0 +1,37 @@
+package game;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import skills.Skill;
+
+public abstract class Entity {
+	//position on board
+	private double x_pos;
+	private double y_pos;
+	
+	//state of the entity
+	private enum state{IDLE,MOVING,USING_SKILL};
+	
+	//hitbox for the entity
+	private Rectangle hitBox;
+	
+	//animation for the entitiy
+	private Animation animation;
+	
+	
+	private EntityRecourse health,stamina,mana;
+	
+	private ArrayList<Skill> skills;
+	
+	//melle damage
+	int strength;
+	//health,stamina 
+	int consitution;
+	//speed
+	int agility;
+	//mana, skill power
+	int inteligence;
+	
+
+}
---
+++ src/game/EntityRecourse.java
@@ -0,0 +1,12 @@
+package game;
+
+//recourses entitys have such as halth, stamina
+public class EntityRecourse {
+	int max;
+	int current;
+	
+	public EntityRecourse(int max) {
+		this.max = max;
+		this.current = current;
+	}
+}
